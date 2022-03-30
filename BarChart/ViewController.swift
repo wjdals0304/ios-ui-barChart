e89896520047001a7afb02fe2835366eea6556c6
jmkim <wjdals0304@gmail.com>
2022-03-30 15:26:49 +0900

    add: file add

--- BarChart/ViewController.swift
+++ BarChart/ViewController.swift
@@ -8,12 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var barStackView: UIStackView!
+    private var values: [CGFloat] = []
+    private var horizontalRange = (1...12)
+    private var barHeightConstraints: [NSLayoutConstraint] = []
+    
+    private var barHeightMultipliers:[CGFloat] {
+        values.map { $0 / 130 }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        randomizeValue()
+        drawBars()
+        
+        addConstraint()
+        
     }
-
+    
+    
+    @IBAction func touchRandomButton() {
+        randomizeValue()
+        addConstraint()
+        
+        UIView.animate(withDuration: 0.3){
+            self.view.layoutIfNeeded()
+        }
+        
+                    
+    }
+    
+    private func addConstraint(){
+        barHeightConstraints.forEach { constraint in
+            constraint.isActive = false
+        }
+                
+        barHeightConstraints = zip(barHeightMultipliers, barStackView.arrangedSubviews).map({
+            $1.heightAnchor.constraint(equalTo: barStackView.heightAnchor, multiplier: $0)
+        })
+        
+        NSLayoutConstraint.activate(barHeightConstraints)
+    }
+    
+    
+    private func randomizeValue() {
+        values = horizontalRange.map({ _ in
+            CGFloat.random(in: 0...100)
+        })
+    }
+    
+    
+    private func drawBars() {
+        horizontalRange.forEach { x in
+            let bar = UIView()
+            bar.backgroundColor = .brown
+            barStackView.addArrangedSubview(bar)
+        }
+    }
 
 }
 
